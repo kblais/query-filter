04c52bf36d80a564b7f6d8359d4086b4db1fbdca
Killian Blais <contact@killianblais.fr>
2021-06-15 17:22:11 +0200

    Move to PHPCSFixer 3.* and PSR-12

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,12 +5,13 @@
  * https://mlocati.github.io/php-cs-fixer-configurator/#version:2.15.1|configurator
  * you can change this configuration by importing this file.
  */
-return PhpCsFixer\Config::create()
-    ->setRiskyAllowed(true)
+$config = new PhpCsFixer\Config();
+
+return $config->setRiskyAllowed(true)
     ->setRules([
         '@PSR1' => true,
         '@PhpCsFixer' => true,
-        '@PSR2' => true,
+        '@PSR12' => true,
         '@PhpCsFixer:risky' => true,
     ])
     ->setFinder(PhpCsFixer\Finder::create()
